ba311003d184bf3dce21bb94960ff75b17ce0edf
Enzo Lizama <lizama.enzo@gmail.com>
2019-10-04 00:33:28 -0500

    main entry of the cli

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "flag"
 
+func main() {
+	var folder string
+	var email string
+
+	flag.StringVar(&folder, "add", "", "add a new folder to scan for git repositories")
+	flag.StringVar(&email, "email", "your@email.com", "the email to scan")
+
+	if folder != "" {
+		scan(folder)
+		return
+	}
+	stats(email)
 }
